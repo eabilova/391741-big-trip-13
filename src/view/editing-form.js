c6cd71ba1b8c64ddaf2073ab566b0cd4f141cf74
Elena Abilova <lena.abilova@gmail.com>
2020-11-25 22:58:21 +0300

    Fixed the identifySelectedOffers function

--- src/view/editing-form.js
+++ src/view/editing-form.js
@@ -11,7 +11,7 @@ const editingEventTypeFormTemplate = (currentType) => {
 
 const identifySelectedOffers = (currentOffers) => {
   return OFFERS.map((offer) => `<div class="event__offer-selector">
-          <input class="event__offer-checkbox  visually-hidden" id="event-offer-${offer.id}-1" type="checkbox" name="event-offer-${offer.id}" ${currentOffers.includes(offer.id) ? `checked` : ``}>
+          <input class="event__offer-checkbox  visually-hidden" id="event-offer-${offer.id}-1" type="checkbox" name="event-offer-${offer.id}" ${currentOffers.find((currentOffer) => currentOffer.id === offer.id) ? `checked` : ``}>
           <label class="event__offer-label" for="event-offer-${offer.id}-1">
             <span class="event__offer-title">${offer.offerName}</span>
             &plus;&euro;&nbsp;
@@ -93,50 +93,6 @@ export const editingFormTemplate = (point = {}) => {
 
       <div class="event__available-offers">
       ${checkOffers}
-<!--        <div class="event__offer-selector">-->
-<!--          <input class="event__offer-checkbox  visually-hidden" id="event-offer-luggage-1" type="checkbox" name="event-offer-luggage" checked>-->
-<!--          <label class="event__offer-label" for="event-offer-luggage-1">-->
-<!--            <span class="event__offer-title">Add luggage</span>-->
-<!--            &plus;&euro;&nbsp;-->
-<!--            <span class="event__offer-price">50</span>-->
-<!--          </label>-->
-<!--        </div>-->
-
-<!--        <div class="event__offer-selector">-->
-<!--          <input class="event__offer-checkbox  visually-hidden" id="event-offer-comfort-1" type="checkbox" name="event-offer-comfort" checked>-->
-<!--          <label class="event__offer-label" for="event-offer-comfort-1">-->
-<!--            <span class="event__offer-title">Switch to comfort</span>-->
-<!--            &plus;&euro;&nbsp;-->
-<!--            <span class="event__offer-price">80</span>-->
-<!--          </label>-->
-<!--        </div>-->
-
-<!--        <div class="event__offer-selector">-->
-<!--          <input class="event__offer-checkbox  visually-hidden" id="event-offer-meal-1" type="checkbox" name="event-offer-meal">-->
-<!--          <label class="event__offer-label" for="event-offer-meal-1">-->
-<!--            <span class="event__offer-title">Add meal</span>-->
-<!--            &plus;&euro;&nbsp;-->
-<!--            <span class="event__offer-price">15</span>-->
-<!--          </label>-->
-<!--        </div>-->
-
-<!--        <div class="event__offer-selector">-->
-<!--          <input class="event__offer-checkbox  visually-hidden" id="event-offer-seats-1" type="checkbox" name="event-offer-seats">-->
-<!--          <label class="event__offer-label" for="event-offer-seats-1">-->
-<!--            <span class="event__offer-title">Choose seats</span>-->
-<!--            &plus;&euro;&nbsp;-->
-<!--            <span class="event__offer-price">5</span>-->
-<!--          </label>-->
-<!--        </div>-->
-
-<!--        <div class="event__offer-selector">-->
-<!--          <input class="event__offer-checkbox  visually-hidden" id="event-offer-train-1" type="checkbox" name="event-offer-train">-->
-<!--          <label class="event__offer-label" for="event-offer-train-1">-->
-<!--            <span class="event__offer-title">Travel by train</span>-->
-<!--            &plus;&euro;&nbsp;-->
-<!--            <span class="event__offer-price">40</span>-->
-<!--          </label>-->
-<!--        </div>-->
       </div>
     </section>
 
